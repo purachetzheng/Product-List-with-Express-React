a3964afd5fd476f3c0833a8a2417631efd94b1dc
Purachet Zheng <poorachad2545@gmail.com>
2022-01-17 03:58:36 +0700

    first server push

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    // password: '',
+    database: 'productlist',
+});
+
+app.get('/products', (req, res) => {
+    db.query('SELECT * FROM products', (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.send(result)
+        };
+    })
+})
+
+app.listen("5000", () => {
+    console.log("Server is running on port 5000");
+});
